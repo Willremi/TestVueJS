355f78ba908f1c2d8156a1802dba54f957c87328
Rémi Willebrouck <willremi@gmail.com>
2018-12-28 20:14:32 +0100

    udapte file index.html 12/28/2018

--- js/script.js
+++ js/script.js
@@ -11,3 +11,24 @@ var app2 = new Vue({
     message: 'Vous avez affiché cette page le ' + new Date().toLocaleString()
   }
 })
+// app2.message = 'un nouveau message'
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+// app3.seen = false
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Apprendre JavaScript' },
+      { text: 'Apprendre Vue' },
+      { text: 'Créer quelque chose de génial' }
+    ]
+  }
+})
+// app4.todos.push({ text: 'Un nouvel élément ajouté' })
