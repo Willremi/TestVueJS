ee478ce68fe65029000ae0a0ae7503da108c4b3c
Rémi Willebrouck <willremi@gmail.com>
2019-01-01 19:41:28 +0100

    add file time.html

---
+++ js/time.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    seconds: 0
+  },
+  mounted: function () {
+    this.$interval = setInterval(() => {
+      console.log('Time')
+      this.seconds++
+    }, 1000)
+  },
+  destroyed: function () {
+    clearInterval(this.$interval)
+  }
+})
