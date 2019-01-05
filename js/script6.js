62a54a1d6c896787927a012d0d56ac958bf17cce
Rémi Willebrouck <willremi@gmail.com>
2019-01-05 15:00:46 +0100

    add files index5.html and script6.js

---
+++ js/script6.js
@@ -0,0 +1,48 @@
+// Vue.component('message', {
+  // props: ['type', 'message'],
+//   props: {
+//     type: {type: String, default: 'success'},
+//     message: String
+//   },
+//   template: `<div class="ui message success" :class="type">{{ message }}</div>`
+// })
+let message = {
+  props: {
+      type: {type: String, default: 'success'},
+      message: String
+    },
+    template: `<div class="ui message success" :class="type">{{ message }}</div>`
+}
+
+let counter = {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  props: {
+    start: {type: Number, default: 0}
+  },
+  computed: {
+    total: function () {
+      return this.start + this.count
+    }
+  },
+  methods: {
+    increment: function () {
+      this.count ++
+    }
+  },
+  template: `<button @click="increment">{{ total }}</button>`
+  // mounted: function () {
+  //   this.count = this.start
+  // }
+}
+
+let app = new Vue({
+  el: '#app',
+  components: { message, counter },
+  data: {
+    message: 'Salut les amis'
+  }
+})
